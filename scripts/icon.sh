7fbd9aff00994cffb6991a5959c31a7d0d110f34
Joe Mo <49494600+yuzhoumo@users.noreply.github.com>
2022-03-27 12:59:27 -0700

    Remove unused lines from icon.sh

--- scripts/icon.sh
+++ scripts/icon.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env zsh
 # Set application icons to match Big Sur style
-# https://apple.stackexchange.com/q/410741
 # by Yuzhou (Joe) Mo <joe.mo@berkeley.edu>
 # License: GNU GPLv3
 
@@ -21,13 +20,11 @@ apps=(
   "zoom.us"
 )
 
-launchservices_path="/System/Library/Frameworks/CoreServices.framework/Versions/A/Frameworks/LaunchServices.framework/Versions/A/Support/lsregister"
 icon_dir="../assets/icons"
-
 for app_name in "${apps[@]}"; do
   echo "Setting app icon: $app_name"
 
-  # Fetch the name of the .icns file to replace, and add .icns extension if it does not have one
+  # Fetch the name of the icon file to replace and add .icns extension if it does not have one
   icon_name=`defaults read "/Applications/${app_name}.app/Contents/Info" CFBundleIconFile`
   [ ${icon_name: -5} != ".icns" ] && icon_name+=".icns"
   
