1245d7eb14a8c2801e84be00fb43a6a4207349bd
Joe Mo <49494600+yuzhoumo@users.noreply.github.com>
2022-04-08 21:37:24 -0700

    Add autorun script

---
+++ run/auto.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env zsh
+# Run automated setup scripts
+# Author: Yuzhou "Joe" Mo (@yuzhoumo)
+# License: GNU GPLv3
+
+printf "\n\x1b[33mAUTOMATED SETUP\x1b[0m\n\n"
+
+# Launch system settings automation
+printf "\n\x1b[33m### Apply automated system settings\x1b[0m\n\n"
+../scripts/macos.sh
+
+# Launch brew automation
+printf "\n\x1b[33m### Install packages from Homebrew\x1b[0m\n\n"
+../scripts/brew.sh
+
+# Launch icon automation
+printf "\n\x1b[33m### Set application icons\x1b[0m\n\n"
+../scripts/icon.sh
+
+# Launch dock automation
+printf "\n\x1b[33m### Set macOS dock\x1b[0m\n\n"
+../scripts/dock.sh
+
+# Launch post-install automation
+printf "\n\x1b[33m### Set various post-install tweaks\x1b[0m\n\n"
+../scripts/post.sh
+
+printf "\n\x1b[32mDone with automated setup.\x1b[0m\n\n"
---
+++ run/manual.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env zsh
+# Run interative manual setup
+# Author: Yuzhou "Joe" Mo (@yuzhoumo)
+# License: GNU GPLv3
+
+prompt_continue() {
+  printf "\n🚀 \x1b[32mPress ENTER to continue...\x1b[0m" && read
+}
+
+printf "\n\x1b[33mMANUAL SETUP\x1b[0m\n\n"
+
+# Setup full disk encryption
+printf "\n\x1b[33m### Enable full disk encryption via FileVault\x1b[0m\n\n"
+touch ~/Desktop/filevault-setup-output.txt
+sudo fdesetup enable >> ~/Desktop/filevault-setup-output.txt
+printf "Output was saved to the desktop!\n"
+prompt_continue
+
+# Setup Spotlight
+printf "\n\x1b[33m### System Preferences > Spotlight > Search Results\x1b[0m\n\n"
+open /System/Library/PreferencePanes/Spotlight.prefPane
+printf "Check the following options:\n\n"
+printf "* Applications\n* Folders\n* Fonts\n* PDF Documents\n* System Preferences\n"
+prompt_continue
+
+# Setup Security & Privacy
+printf "\n\x1b[33m### System Preferences > Security & Privacy > Privacy\x1b[0m\n\n"
+open /System/Library/PreferencePanes/Security.prefPane
+printf "Setup privacy settings as needed\n"
+prompt_continue
+
+# Setup Extensions
+printf "\n\x1b[33m### System Preferences > Extensions\x1b[0m\n\n"
+open /System/Library/PreferencePanes/Extensions.prefPane
+printf "Uncheck unecessary extensions\n"
+prompt_continue
+
+# Setup General
+printf "\n\x1b[33m### System Preferences > General\x1b[0m\n\n"
+open /System/Library/PreferencePanes/Appearance.prefPane
+printf "Set \"Recent Items\" to \"none\"\n"
+prompt_continue
+
+# Reduce Transparency
+printf "\n\x1b[33m### System Preferences > Accessibility > Display\x1b[0m\n\n"
+open /System/Library/PreferencePanes/UniversalAccessPref.prefPane
+printf "Check \"Reduce Transparency\"\n"
+prompt_continue
+
+# Close System Preferences
+osascript -e 'tell application "System Preferences" to quit'
+
+# Finder sidebar
+printf "\n\x1b[33m### Menu Bar > Finder > Preferences > Sidebar\x1b[0m\n\n"
+open /
+printf "Uncheck the following:\n\n"
+printf "* iCloud Drive\n* Cloud Storage\n* Bonjour Computers\n"
+prompt_continue
+
+# Close Finder window
+osascript -e 'tell application "Finder" to close windows'
+
+printf "\n\x1b[32mDone with manual setup.\x1b[0m\n\n"
