6335c8886afae2006ce8b08e1379a6439a34f324
Joe Mo <49494600+yuzhoumo@users.noreply.github.com>
2022-04-14 14:09:49 -0700

    Remove auto/manual setup split shell feature

--- run/run.sh
+++ run/run.sh
@@ -6,24 +6,20 @@
 printf "\n\x1b[33mWelcome to Joe's interactive setup script for macOS!\x1b[0m\n\n"
 printf "This script will guide you through manual setup steps and launch\n"
 printf "automated configurations for system settings and personalization.\n"
-printf "\n🚀 \x1b[32mPress ENTER to continue...\x1b[0m" && read
+printf "\n🚀 \x1b[32mPress ENTER to continue to automated setup...\x1b[0m" && read
 
 # Navigate to current directory
 cd "$(dirname "${0}")" || exit
 
-# Install homebrew if not already installed
-command -v brew >/dev/null 2>&1 || /bin/bash -c \
-  "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" && \
-  osascript -e 'set volume output volume 50' && say "Homebrew installation completed"
+# Run all automated configurations
+printf "\nLaunching automated setup...\n"
+./auto.sh
 
-# Install tmux if not already installed
-command -v tmux >/dev/null 2>&1 || brew install tmux || exit
+# Set volume to 50% and alert user
+osascript -e 'set volume output volume 50' && say "Automated setup complete."
 
-printf "\nLaunching split window...\n"
-
-# Run both scripts in split window
-tmux \
-  new-session  './manual.sh; echo "Exit in 10 seconds..."; sleep 10' \; \
-  split-window './auto.sh; echo "Exit in 10 seconds..."; sleep 10' > /dev/null
+# Run interactive guide for manually configured settings
+printf "\n🚀 \x1b[32mPress ENTER to continue to manual setup...\x1b[0m" && read
+./manual.sh
 
 printf "\n\x1b[32mSetup complete. Please restart your computer.\x1b[0m\n\n"
